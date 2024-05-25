f870f483ecfcd5977c06bd92a9a2708121ac7045
jeffolmo <jeffreyjolmo@gmail.com>
2024-05-25 16:38:52 -0600

    Refactored and fixed some problems

--- server/src/main/java/service/GameService.java
+++ server/src/main/java/service/GameService.java
@@ -9,18 +9,16 @@ import util.CodedException;
 import java.util.Collection;
 
 public class GameService {
-
     private final DataAccess dataAccess;
 
     public GameService(DataAccess dataAccess) {
         this.dataAccess = dataAccess;
     }
 
-
     public Collection<GameData> listGames() throws CodedException {
         try {
             return dataAccess.listGames();
-        } catch (DataAccessException ex) {
+        } catch (DataAccessException ignored) {
             throw new CodedException(500, "Server error");
         }
     }
@@ -28,40 +26,43 @@ public class GameService {
     public GameData createGame(String gameName) throws CodedException {
         try {
             return dataAccess.newGame(gameName);
-        } catch (DataAccessException ex) {
-            throw new CodedException(500, "Server error");
-        }
-    }
-
-    public GameData joinGame(String username, ChessGame.TeamColor color, int gameID) throws CodedException {
-        try {
-            var gameData = dataAccess.readGame(gameID);
-            if (gameData == null) {
-                throw new CodedException(400, "Unknown game");
-            } else if (color == null) {
-                return gameData;
-            } else if (gameData.isGameOver()) {
-                throw new CodedException(403, "Game is over");
-            } else {
-                if (color == ChessGame.TeamColor.WHITE) {
-                    if (gameData.whiteUsername() == null || gameData.whiteUsername().equals(username)) {
-                        gameData = gameData.setWhite(username);
-                    } else {
-                        throw new CodedException(403, "Color taken");
-                    }
-                } else if (color == ChessGame.TeamColor.BLACK) {
-                    if (gameData.blackUsername() == null || gameData.blackUsername().equals(username)) {
-                        gameData = gameData.setBlack(username);
-                    } else {
-                        throw new CodedException(403, "Color taken");
-                    }
-                }
-                dataAccess.updateGame(gameData);
-            }
-            return gameData;
         } catch (DataAccessException ignored) {
             throw new CodedException(500, "Server error");
         }
     }
 
+    public GameData joinGame(String username, ChessGame.TeamColor color, int gameId) throws CodedException {
+        try {
+            GameData loadedGame = dataAccess.readGame(gameId);
+            if (loadedGame == null) {
+                throw new CodedException(400, "Unknown game");
+            } else if (color == null) {
+                return loadedGame;
+            } else if (loadedGame.isGameOver()) {
+                throw new CodedException(403, "Game is over");
+            } else {
+                assignColorToPlayer(username, color, loadedGame);
+                dataAccess.updateGame(loadedGame);
+            }
+            return loadedGame;
+        } catch (DataAccessException ignored) {
+            throw new CodedException(500, "Server error");
+        }
+    }
+
+    private void assignColorToPlayer(String username, ChessGame.TeamColor color, GameData loadedGame) throws CodedException {
+        if (color == ChessGame.TeamColor.WHITE) {
+            if (loadedGame.whiteUsername() == null || loadedGame.whiteUsername().equals(username)) {
+                loadedGame = loadedGame.setWhite(username);
+            } else {
+                throw new CodedException(403, "Color taken");
+            }
+        } else if (color == ChessGame.TeamColor.BLACK) {
+            if (loadedGame.blackUsername() == null || loadedGame.blackUsername().equals(username)) {
+                loadedGame = loadedGame.setBlack(username);
+            } else {
+                throw new CodedException(403, "Color taken");
+            }
+        }
+    }
 }
