016aac0f06d4aba28e709786459923e5215b4184
jeffolmo <jeffreyjolmo@gmail.com>
2024-05-25 13:05:10 -0600

    Wrote the DataAccess class

---
+++ server/src/main/java/dataaccess/DataAccess.java
@@ -0,0 +1,31 @@
+package dataaccess;
+
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.Collection;
+
+
+public interface DataAccess {
+    void clear() throws DataAccessException;
+
+
+    UserData writeUser(UserData user) throws DataAccessException;
+
+    UserData readUser(String userName) throws DataAccessException;
+
+    AuthData writeAuth(String username) throws DataAccessException;
+
+    AuthData readAuth(String authToken) throws DataAccessException;
+
+    void deleteAuth(String authToken) throws DataAccessException;
+
+    GameData newGame(String gameName) throws DataAccessException;
+
+    void updateGame(GameData game) throws DataAccessException;
+
+    GameData readGame(int gameID) throws DataAccessException;
+
+    Collection<GameData> listGames() throws DataAccessException;
+}
