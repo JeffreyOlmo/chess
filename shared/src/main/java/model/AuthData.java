f902f256f180600370a9211bda295d710e872ad3
jeffolmo <jeffreyjolmo@gmail.com>
2024-05-25 15:07:24 -0600

    Updated the pom file and added the CodedException method

--- shared/src/main/java/model/AuthData.java
+++ shared/src/main/java/model/AuthData.java
@@ -1,3 +1,18 @@
 package model;
 
-public record AuthData(String authToken, String username) {}
+import com.google.gson.Gson;
+import java.util.UUID;
+
+/**
+ * Associates a user and a unique token to demonstrate the user's authorization state.
+ */
+public record AuthData(String authToken, String username) {
+    public static String generateToken() {
+        return UUID.randomUUID().toString();
+    }
+
+    @Override
+    public String toString() {
+        return new Gson().toJson(this);
+    }
+}
